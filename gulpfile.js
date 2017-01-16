167ed955a4296b88c69820331fe5d7ec03b7ece1
rinathafiyatullin <rhafiyatullin@gmail.com>
2017-01-16 15:17:59 +0300

    Adds minification of images using gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var nodemon = require('gulp-nodemon');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var htmlmin = require('gulp-html-minifier');
+var imagemin = require('gulp-imagemin');
 var swig = require('gulp-swig');
 var rename = require('gulp-rename');
 var json = require('./app/json/data.json');
@@ -13,9 +14,10 @@ var json = require('./app/json/data.json');
 var stylesPath = 'app/assets/styles/*.{styl,css}';
 var devStylesPath = 'app/assets/dev/*.styl';
 var scriptsPath = 'app/assets/scripts/*.js';
+var imagePath = 'app/assets/images/*';
 var templatesPath = './views/*.html';
 
-gulp.task('styles', function () {
+gulp.task('styles', function() {
     return gulp
         .src(stylesPath)
         .pipe(stylus({
@@ -40,7 +42,13 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('./public/scripts'));
 });
 
-gulp.task('devStyles', function () {
+gulp.task('imageMin', function() {
+    return gulp.src(imagePath)
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/img'))
+});
+
+gulp.task('devStyles', function() {
     return gulp
         .src(devStylesPath)
         .pipe(stylus())
@@ -48,30 +56,30 @@ gulp.task('devStyles', function () {
         .pipe(gulp.dest('./public/styles'));
 });
 
-gulp.task('templates', function() {
-    gulp.src(templatesPath)
+gulp.task('templates', ['styles', 'scripts'],  function() {
+    return gulp.src(templatesPath)
         .pipe(swig({defaults: { cache: false }, data:json}))
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('app/assets/**/*.{styl,css}', ['styles']);
     gulp.watch(devStylesPath, ['devStyles']);
     gulp.watch(templatesPath, ['templates']);
     gulp.watch(scriptsPath, ['scripts']);
 });
 
-gulp.task('startServer', function () {
+gulp.task('startServer', function() {
     nodemon({
         script: 'server.js',
         ext: 'html js'
     })
-    .on('restart', function () {
+    .on('restart', function() {
         console.log('restarted!')
     })
 });
 
-gulp.task('build', ['devStyles', 'styles', 'scripts', 'templates']);
+gulp.task('build', ['imageMin', 'devStyles', 'styles', 'scripts', 'templates']);
 
 gulp.task('default', ['build', 'startServer', 'watch']);
